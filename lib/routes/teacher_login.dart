5e0731d037ee8e3400bda48ddf7b8d8928bc3059
Yogesh Singla <yogeshsingla4@gmail.com>
2020-12-22 21:46:37 +0530

    updated auth for teacher

--- lib/routes/teacher_login.dart
+++ lib/routes/teacher_login.dart
@@ -1,61 +1,129 @@
+import 'package:TIES/routes/teacher_profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:TIES/services/auth.dart';
+
 class Teacher extends StatefulWidget {
   @override
   _TeacherState createState() => _TeacherState();
 }
 
 class _TeacherState extends State<Teacher> {
-  
+  final _key = GlobalKey<FormState>();
+  String useremail;
+  String password;
+  String user = " ";
+  bool credentialTrue;
   TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    credentialTrue = true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-              child: Column(
-                children: [
-          SizedBox(height: 40,),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: TextFormField(
-              controller: _textEditingController,
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                hintText: 'Email-Id',
+      child: Form(
+        key: _key,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: TextFormField(
+                  key: ValueKey('email'),
+                  validator: (value) {
+                    if (value.isEmpty)
+                      return 'Enter an Email address';
+                    else if (!value.contains('@'))
+                      return 'Please enter a valid email address';
+                    return null;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _textEditingController,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Email-Id',
+                  ),
+                  onSaved: (value) {
+                    useremail = value;
+                  },
+                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: TextFormField(
+                controller: _textEditingController,
+                validator: (value) {
+                  if (value.isEmpty) return "Enter a Password";
+                  return null;
+                },
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+                onSaved: (value) {
+                  password = value;
+                },
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal:15.0),
-            child: TextFormField(
-              controller: _textEditingController,
-              decoration: InputDecoration(
-                fillColor: Colors.white,
-                filled: true,
-                hintText: 'Password',
-                
+            if (credentialTrue == false)
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 3),
+                margin: EdgeInsets.only(right: 256),
+                child: Text(
+                  user,
+                  style: TextStyle(color: Colors.red),
+                  textAlign: TextAlign.left,
+                ),
               ),
-              obscureText: true,
+            SizedBox(
+              height: 20,
             ),
-          ),
-          SizedBox(height: 20,),
-          CircleAvatar(
-            radius: 35,
-            backgroundColor: Colors.red,
-                      child: IconButton(
-              
-              onPressed: () {
+            CircleAvatar(
+              radius: 35,
+              backgroundColor: Colors.red,
+              child: IconButton(
+                onPressed: () async {
+                  _key.currentState.save();
+                  bool answer = _key.currentState.validate();
+                  if (answer) {
+                    user = await Authentication()
+                        .signIn(email: useremail, password: password);
 
-              },
-              icon: Icon(Icons.arrow_forward),
-              iconSize: 50,
-              color: Colors.white,
+                    if (user == "Yes") {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TeacherProfilePage()));
+                    } else {
+                      setState(() {
+                        credentialTrue = false;
+                      });
+                    }
+                  }
+                },
+                icon: Icon(Icons.arrow_forward),
+                iconSize: 50,
+                color: Colors.white,
+              ),
             ),
-          ),
-          SizedBox(height: 55,),
-          Text('Forgot Passsword',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,fontSize: 19),),
-        ],),
-      
-   
+            SizedBox(
+              height: 55,
+            ),
+            Text(
+              'Forgot Passsword',
+              style: TextStyle(
+                  color: Colors.red, fontWeight: FontWeight.bold, fontSize: 19),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
